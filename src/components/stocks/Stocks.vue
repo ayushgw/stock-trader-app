9b149b34ffa78f47c036adf00425507dd1b6ea05
ayushgw <ayushgoswami@zoho.com>
2019-07-23 23:55:53 +0530

    added vuex store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
 import Stock from './Stock.vue';
 
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 100 },
-        { id: 2, name: "Google", price: 190 },
-        { id: 3, name: "Twitter", price: 200 },
-        { id: 4, name: "OpIndia", price: 500 }
-      ]
-    };
-  },
   components: {
       appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    }
   }
 };
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: "BMW", price: 100 },
+    { id: 2, name: "Google", price: 190 },
+    { id: 3, name: "Twitter", price: 200 },
+    { id: 4, name: "OpIndia", price: 500 }
+]
